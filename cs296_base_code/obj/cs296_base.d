ccef0f0b8034cb663e4877b87d6cf086e38ee385
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2014-04-10 08:41:59 +0530

    updated makefile with dist and install targets

---
+++ cs296_base_code/obj/cs296_base.d
@@ -0,0 +1,118 @@
+/home/dheerendra/Desktop/project/cs296_base_code/obj/cs296_base.o: \
+ /home/dheerendra/Desktop/project/cs296_base_code/src/cs296_base.cpp \
+ /home/dheerendra/Desktop/project/cs296_base_code/src/cs296_base.hpp \
+ /home/dheerendra/Desktop/project/cs296_base_code/src/render.hpp \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Box2D.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Common/b2Settings.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stddef.h /usr/include/assert.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/bits/predefs.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/float.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Common/b2Draw.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Common/b2Math.h \
+ /usr/include/math.h /usr/include/x86_64-linux-gnu/bits/huge_val.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_valf.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_vall.h \
+ /usr/include/x86_64-linux-gnu/bits/inf.h \
+ /usr/include/x86_64-linux-gnu/bits/nan.h \
+ /usr/include/x86_64-linux-gnu/bits/mathdef.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathinline.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Common/b2Timer.h \
+ /usr/include/x86_64-linux-gnu/sys/time.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/sigset.h \
+ /usr/include/x86_64-linux-gnu/bits/select2.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Collision/Shapes/b2CircleShape.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Collision/Shapes/b2Shape.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Common/b2BlockAllocator.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Collision/b2Collision.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Collision/Shapes/b2EdgeShape.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Collision/Shapes/b2ChainShape.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Collision/Shapes/b2PolygonShape.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Collision/b2BroadPhase.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Collision/b2DynamicTree.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Common/b2GrowableStack.h \
+ /usr/include/memory.h /usr/include/string.h /usr/include/xlocale.h \
+ /usr/include/x86_64-linux-gnu/bits/string3.h \
+ /usr/include/c++/4.6/algorithm /usr/include/c++/4.6/utility \
+ /usr/include/c++/4.6/x86_64-linux-gnu/./bits/c++config.h \
+ /usr/include/c++/4.6/x86_64-linux-gnu/./bits/os_defines.h \
+ /usr/include/c++/4.6/x86_64-linux-gnu/./bits/cpu_defines.h \
+ /usr/include/c++/4.6/bits/stl_relops.h \
+ /usr/include/c++/4.6/bits/stl_pair.h /usr/include/c++/4.6/bits/move.h \
+ /usr/include/c++/4.6/bits/concept_check.h \
+ /usr/include/c++/4.6/bits/stl_algobase.h \
+ /usr/include/c++/4.6/bits/functexcept.h \
+ /usr/include/c++/4.6/bits/exception_defines.h \
+ /usr/include/c++/4.6/bits/cpp_type_traits.h \
+ /usr/include/c++/4.6/ext/type_traits.h \
+ /usr/include/c++/4.6/ext/numeric_traits.h \
+ /usr/include/c++/4.6/bits/stl_iterator_base_types.h \
+ /usr/include/c++/4.6/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/4.6/bits/stl_iterator.h \
+ /usr/include/c++/4.6/debug/debug.h /usr/include/c++/4.6/bits/stl_algo.h \
+ /usr/include/c++/4.6/cstdlib /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib.h \
+ /usr/include/c++/4.6/bits/algorithmfwd.h \
+ /usr/include/c++/4.6/initializer_list \
+ /usr/include/c++/4.6/bits/stl_heap.h \
+ /usr/include/c++/4.6/bits/stl_tempbuf.h \
+ /usr/include/c++/4.6/bits/stl_construct.h /usr/include/c++/4.6/new \
+ /usr/include/c++/4.6/exception \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Collision/b2Distance.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Collision/b2TimeOfImpact.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/b2Body.h \
+ /usr/include/c++/4.6/memory /usr/include/c++/4.6/bits/allocator.h \
+ /usr/include/c++/4.6/x86_64-linux-gnu/./bits/c++allocator.h \
+ /usr/include/c++/4.6/ext/new_allocator.h \
+ /usr/include/c++/4.6/bits/stl_uninitialized.h \
+ /usr/include/c++/4.6/bits/stl_raw_storage_iter.h \
+ /usr/include/c++/4.6/backward/auto_ptr.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/b2Fixture.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/b2WorldCallbacks.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/b2TimeStep.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/b2World.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Common/b2StackAllocator.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/b2ContactManager.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Contacts/b2Contact.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2DistanceJoint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2Joint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2FrictionJoint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2GearJoint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2MotorJoint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2MouseJoint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2PrismaticJoint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2PulleyJoint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2RevoluteJoint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2RopeJoint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2WeldJoint.h \
+ /home/dheerendra/Desktop/project/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2WheelJoint.h \
+ /usr/include/c++/4.6/cstdio /usr/include/stdio.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h
